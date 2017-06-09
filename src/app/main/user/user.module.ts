b7f64c17ff320069beb9b3db6098fd2bb8f20442
lymaodt <lymaodt@gmail.com>
2017-06-10 00:22:28 +0700

    route in angular2

--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import { Routes, RouterModule } from '@angular/router'
+
+export const userRountes: Routes = [
+  { path: '', redirectTo: 'user', pathMatch: 'full' },
+  { path: 'user', component: UserComponent }
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(userRountes)
   ],
   declarations: [UserComponent]
 })
